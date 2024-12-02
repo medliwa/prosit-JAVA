f3e9e233db65a742cc85d333243491e8150f8fab
med_liwa <mohamedliwa.belmahdhi@esprit.tn>
2024-12-03 00:10:51 +0100

    teeeeeeest

---
+++ Zoo.java
@@ -0,0 +1,13 @@
+public class Zoo {
+}
+public Animal[] animals;
+public String name;
+public String city;
+public int nbrCages;
+
+public Zoo(string name,String city, int nbrCages){
+    this.name = name;
+    this.city = city;
+    this.nbrCages = nbrCages;
+    this.animals = new Animal[25];
+}
